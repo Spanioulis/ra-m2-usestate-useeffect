cbba78720f6ff33df2ff0a4e0fda3ed8f19d034a
Kevin Mamaqi <kevinmamaqi@gmail.com>
2023-01-15 15:11:02 +0100

    segunda-revision

--- src/components/atoms/Label.jsx
+++ src/components/atoms/Label.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
 
 export const Label = ({ children, info }) => {
-   return (
-      <label>
-         {children} {info}{' '}
-      </label>
-   );
+  return (
+    // falta htmlFor
+    <label>
+      {/* Aquí debería de ser solo children, el info sobra. */}
+      {children} {info}{" "}
+    </label>
+  );
 };
+
+// Utiliza prop-types para validar el tipo de dato de las props
--- src/components/atoms/SquareSVG.jsx
+++ src/components/atoms/SquareSVG.jsx
@@ -1,7 +1,9 @@
 export const SquareSVG = ({ color, size }) => {
-   return (
-      <svg width={`${size}px`} height={`${size}px`}>
-         <rect width={`${size}px`} height={`${size}px`} fill={color} />
-      </svg>
-   );
+  return (
+    <svg width={`${size}px`} height={`${size}px`}>
+      <rect width={`${size}px`} height={`${size}px`} fill={color} />
+    </svg>
+  );
 };
+
+// Utiliza prop-types para validar el tipo de dato de las props
